ad4845bbcdffb7996b1a10293f03540828d56399
Sourav Singh <souravsingh2609@gmail.com>
2024-05-05 05:56:50 +0530

    Task add and disapper on completion

--- src/Component/Table/Table.jsx
+++ src/Component/Table/Table.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
 function Table() {
 
   const [tableTasks, setTableTasks] = useState([{ task: "Ecommerce Application",
-        completed: false }, { task: "Blockchain Development", completed: true },
-        { task: "Java Development", completed: true },
+        completed: false }, { task: "Blockchain Development", completed: false },
+        { task: "Java Development", completed: false },
         { task: "Frappe Development", completed: false },
         { task: "Machine Learning", completed: false },
         { task: "Neural Networks", completed: false },
@@ -12,16 +12,37 @@ function Table() {
 
   ])
 
+  function addTask(event) {
+    event.preventDefault();
+
+    let task = event.target.task.value;
+    if (!task) {
+      alert("Please enter a valid task!");
+      return;
+    }
+    setTableTasks([...tableTasks, { task: task, completed: false }]);
+    event.target.reset();
+  }
+
   function changeTaskStatus(index) {
-    let newTableTasks = [...tableTasks]
-    newTableTasks[index].completed = !newTableTasks[index].completed
-    setTableTasks(newTableTasks)
+    let newTableTasks = [...tableTasks];
+    newTableTasks[index].completed = !newTableTasks[index].completed;
+    setTableTasks(newTableTasks);
+
+    //deletes the current selected task
+    newTableTasks.splice(index, 1);
+    setTableTasks(newTableTasks);
   }
 
   return (
     <div className='container my-3'>
       <h2 className="text-center border-bottom mb-5">Tasks</h2>
       <div className="mx-auto rounded border p-4" >
+
+        <form className="d-flex mb-5" onSubmit={addTask}>
+          <input className="form-control me-2" placeholder="Add New Task" name='task' />
+          <button className="btn btn-outline-light" type="submit">Add</button>
+        </form>
 
         {
           tableTasks.map((tableTask, index) => {
